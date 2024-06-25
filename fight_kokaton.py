61b6f864bd199b254b441ec451c6a2860d2ae0bd
c0b23022bb <c0b23022bb@edu.teu.ac.jp>
2024-06-25 17:35:15 +0900

    追加機能４完了

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -151,7 +151,26 @@ class Score:
     def update(self, screen: pg.Surface):
         self.img = self.fonto.render(f"スコア:{self.score}", self.score, self.color)
         screen.blit(self.img,self.rct)
-        
+
+class Explosion:
+    """
+    爆発エフェクトのクラス
+    """
+    def __init__(self, bomb:"Bomb", life):
+        # 画像を格納するリストの設定
+        self.explosion_img1 = pg.image.load(f"fig/explosion.gif")
+        self.explosion_img2 = pg.transform.flip(self.explosion_img1, True, True)
+        self.effect_list = [self.explosion_img1, self.explosion_img2]
+        self.rct: pg.Rect = self.explosion_img1.get_rect()
+        self.rct.center = bomb.rct.center
+        # 表示時間（爆発時間）lifeを設定
+        self.life = life
+
+    def update(self, screen: pg.Surface):
+        self.life -= 1
+        if self.life > 0:
+            screen.blit(self.effect_list[self.life // 10 % 2], self.rct)
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -160,6 +179,7 @@ def main():
     beam = None
     score = Score()
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
+    effects = []
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -178,21 +198,26 @@ def main():
                 txt = fonto.render("Game Over", True, (255, 0, 0))
                 screen.blit(txt, [WIDTH/2-150, HEIGHT/2])
                 pg.display.update()
-                time.sleep(1)
+                time.sleep(5)
                 return
             
         for i in range(len(bombs)):
             if beam is not None:
                 if bombs[i].rct.colliderect(beam.rct):
+                    effects.append(Explosion(bombs[i], 50))
                     bombs [i] = None
                     beam = None
                     bird.change_img(6, screen)
                     score.score += 1
         score.update(screen)
         bombs = [bomb for bomb in bombs if bomb is not None]
+        effects = [effect for effect in effects if effect.life > 0]
         
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
+
+        for j in effects:
+            j.update(screen)
         if beam is not None:
             beam.update(screen)
         for bomb in bombs:
